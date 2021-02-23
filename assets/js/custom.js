e14d95d8fb2f1dd7c8e31647b14b2ea118bf4b49
Jakub Strojewski <jakub.strojewski@openpayd.com>
2021-02-23 13:04:09 +0000

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+// var transparentDemo = true;
+// var fixedTop = false;
+//
+// $(window).scroll(function(e) {
+//     oVal = ($(window).scrollTop() / 90);
+//     $(".blur").css("opacity", oVal);
+//
+// });
+//
